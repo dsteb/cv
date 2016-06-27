7cd29337e2f59f872c29b4a3e46c3e31dbe3257a
Dmitrii Stebliuk <dsteblyuk@gmail.com>
2016-06-27 22:18:26 +0200

    Add name to resume

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,26 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var bio;
+
+(function() {
+  'use strict';
+
+  bio = {
+    name: 'Dmitrii Stebliuk',
+    role: 'Senior Fullstack Developer',
+    cotacts: {
+      mobile: '(+39) 327-568-43-13',
+      email: 'dsteblyuk@gmail.com',
+      github: 'https://github.com/n43jl',
+      location: 'Milan, Italy'
+    },
+    welcomeMessage: 'Life is study',
+    skills: ['Java', 'SQL', 'HTML5', 'CSS', 'Javascript', 'JQuery', 'Python'],
+    biopic: 'images/fry.jpg',
+  };
+
+  bio.display = function() {
+    var formattedName = HTMLheaderName.replace('%data%', this.name);
+    $('#header').append(formattedName);
+  };
+
+  bio.display();
+})();
